bdc1490f2390759285e391d6d0fcaffad48d8455
Kaua <kaua.ferreiracavalcante@gmail.com>
2024-05-17 22:35:01 -0300

    add style to topics page and add navigation-signed component

---
+++ frontend/src/app/components/navigation-signed/navigation-signed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-signed',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation-signed.component.html',
+  styleUrl: './navigation-signed.component.css'
+})
+export class NavigationSignedComponent {
+
+}
--- frontend/src/app/pages/topics/topics.component.ts
+++ frontend/src/app/pages/topics/topics.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavigationSignedComponent } from '../../components/navigation-signed/navigation-signed.component';
 
 @Component({
   selector: 'app-topics',
   standalone: true,
-  imports: [],
+  imports: [NavigationSignedComponent],
   templateUrl: './topics.component.html',
   styleUrl: './topics.component.css'
 })
